01dc98f1cb608cf183538802833194c10e4081d3
wesleymichael <wesleymichaelpereira@hotmail.com>
2023-04-14 01:20:54 -0300

    Add POST request to /participants route for creating new participant.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import Joi from "joi";
+import dayjs from "dayjs";
 
-//Cria o servidor
 const app = express();
 
 //Config.
@@ -11,8 +12,7 @@ app.use(express.json());
 app.use(cors());
 dotenv.config();
 
-
-//Conexão com o db
+//Connection to mongodb
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 try{
     await mongoClient.connect();
@@ -22,6 +22,39 @@ try{
 }
 const db = mongoClient.db();
 
+//Validation
+const schemaName = Joi.object({
+    name:  Joi.string()
+        .min(1)
+        .required(),
+});
+
+//EndPoints
+app.post("/participants", async (req, res) => {
+    const name = req.body;
+
+    const validation = schemaName.validate(name);
+    if(validation.error){
+        res.status(422).send(validation.error.details[0].message);
+        return;
+    }
+    try{
+        const nameUsed = await db.collection('participants').findOne({name: name});
+        if( nameUsed ) return res.status(409).send('Nome de usuário já existe.');
+
+        await db.collection('participants').insertOne( {name: name, lastStatus: Date.now()} );
+        await db.collection('messages').insertOne({
+            from: name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
+            time: dayjs().format('HH:mm:ss')
+        });
+        res.sendStatus(201);
+    } catch (err){
+        res.status(500).send(err.message);
+    }
+})
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
